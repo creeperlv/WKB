d47e5ab4031cc4f290274224bd10c5fb6b39b6f2
Creeper Lv <creeperlv@users.noreply.github.com>
2025-04-05 23:43:59 +1100

    Added a command to create config file.

--- src/wkb.app/Program.cs
+++ src/wkb.app/Program.cs
@@ -9,6 +9,19 @@ class Program
 	static void Main(string[] args)
 	{
 		var configService = new core.Configuration.ConfigurationService();
+		for (int i = 0; i < args.Length; i++)
+		{
+			var item = args[i];
+			switch (item)
+			{
+				case "--create-config-file":
+					configService.Save();
+					return;
+					break;
+				default:
+					break;
+			}
+		}
 		WkbCore core = new WkbCore(configService);
 
 		core.Start();
